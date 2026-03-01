a3f187c0c2a65b9f9d566159baa308ed8a2e6643
-XO-OX- <greshamd27@gmail.com>
2026-03-02 06:56:31 +0700

    Basic updates

--- pixel/connect4.py
+++ pixel/connect4.py
@@ -4,6 +4,7 @@ class Game:
         self.cols = cols
         self.board = [[0 for _ in range(cols)] for _ in range(rows)]
         self.last_move = None
+        self.winner = None
 
     def possible_moves(self):
         return [c for c in range(self.cols) if self.board[0][c] == 0]
@@ -23,8 +24,19 @@ class Game:
         for r in range(self.rows):
             for c in range(self.cols):
                 if all(self.board[r][c+i] == player for i in range(4) if c+i < self.cols) or all(self.board[r+i][c] == player for i in range(4) if r+i < self.rows) or all(self.board[r+i][c+i] == player for i in range(4) if r+i < self.rows and c+i < self.cols) or all(self.board[r-i][c-i] == player for i in range(4) if r-i >= 0 and c-i >= 0):
-                    return True
-        return False
+                    return {'success': True}
+        return {'success': False}
 
     def get_state(self):
         return tuple(self.board[r][c] for r in range(self.rows) for c in range(self.cols))
+    
+    def is_terminal(self,player):
+        result = self.check_win(player)
+        if result['success']:
+            self.winner = f'{player}'
+            return {'end': True, 'winner': f'{player}'}
+        elif self.is_full():
+            self.winner = None
+            return {'end': True, 'result': 'Draw'}
+        else:
+            return {'end': False}
--- pixel/main.py
+++ pixel/main.py
@@ -3,11 +3,15 @@ from q_learning import QLearning
 from socket import Socket
 
 class Play:
-    def __init__(self):
+    def __init__(self,p1,p2):
         self.game = Game()
+        self.p1 = p1
+        self.p2 = p2
+        self.current = p1
 
-    def reward(self,board,player):
-        if self.game.is_over():
+    def reward(self,player='Pixel'):
+        result = self.game.is_terminal(player)
+        if result['end']:
             winner = self.game.winner
             if winner == player:
                 return 1.0
@@ -18,20 +22,22 @@ class Play:
         else:
             return 0.01
         
+    def swap_turns(self):
+        self.current = 'smartBot' if self.current == 'Pixel' else 'Pixel'
+        
     def play_game(self,training=True):
-        game = Game()
         ql = QLearning()
         ql.load_Q()
         socket = Socket("moves.txt")
-        while not game.is_over():
-            state = game.get_state()
-            valid_moves = game.possible_moves()
+        while not self.game.is_over():
+            state = self.game.get_state()
+            valid_moves = self.game.possible_moves()
             if training:
                 move = ql.choose_moves(state,valid_moves,training=True)
             else:
-                move = socket.get_move(game.turn)
-            game.make_move(move,game.turn)
-            reward = self.reward(game.board,game.turn)
-            next_state = game.get_state()
-            ql.update_Q(state,move,reward,next_state,game.possible_moves())
+                move = socket.get_move(self.game.turn)
+            self.game.make_move(move,self.game.turn)
+            reward = self.reward(self.game.board,self.game.turn)
+            next_state = self.game.get_state()
+            ql.update_Q(state,move,reward,next_state,self.game.possible_moves())
         ql.save_Q()
